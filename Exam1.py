fc7f096c90874324afc36839d49e10998196b841
Kim JI woon <kishe56@gmail.com>
2017-03-07 20:04:05 +0900

    Exam1 변수 선언, 함수 선언, 변수 스코프, 변수 삭제

--- Exam1.py
+++ Exam1.py
@@ -1,4 +1,40 @@
 import sys
+
+
 print(sys.version)
 print("Hello Python")
 print("Very Simple")
+
+a = 1
+b = 3
+
+print(a+b)
+helloString = "Hello Python"
+print("a+"+helloString+" dosen't work")
+print("str(a)+"+helloString+" working")
+print(str(a)+helloString)
+
+c = a*b
+print(str(a)+" * "+str(b)+"="+str(c))
+c = a/b
+print(str(a)+" / "+str(b)+"="+str(c))
+c = a % b
+print(str(a)+" % "+str(b)+"="+str(c))
+c -= a
+print("c -= a ="+str(c))
+
+del c
+print("c is deleted.")
+
+
+# 함수 선언시(def) 이전에 2개의 개행필요
+def MyFirstFunction(h):
+    print("MyFirstFunction Called")
+    print(h)
+
+
+MyFirstFunction(a)
+MyFirstFunction(b)
+
+# 지역 변수 전역 변수 같음
+# print(h)에서 h는 undefined임
